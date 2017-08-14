a9eb0df9b0d9ae97be6475af2d2a5eaa0dc4c4e1
Ning Yu <ningyu321@gmail.com>
2017-08-15 02:46:58 +0800

    corrected static in server

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static());
+app.use(express.static(__dirname));
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`);
